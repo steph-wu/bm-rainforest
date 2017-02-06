213af69e53dccecfe2bb43eeb61e738e1795e649
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-06 17:25:18 -0500

    Create user model, controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206204608) do
+ActiveRecord::Schema.define(version: 20170206220724) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170206204608) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.text     "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
